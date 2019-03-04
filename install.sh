5eaf72ec374038bed174ca3791f7a6e515145029
oscillo <kasshisatari@gmail.com>
2019-03-04 21:36:49 +0900

    Fix #35.

--- install.sh
+++ install.sh
@@ -42,6 +42,22 @@ sudo ed /etc/default/hostapd << EOF
 wq
 EOF
 
+sudo ed /etc/sysctl.conf << EOF
+%s/#net.ipv4.ip_forward=1/net.ipv4.ip_forward=1/
+wq
+EOF
+
+sudo iptables -t nat -A  POSTROUTING -o eth0 -j MASQUERADE
+sudo sh -c "iptables-save > /etc/iptables.ipv4.nat"
+
+sudo ed /etc/rc.local << EOF
+n
+i
+iptables-restore < /etc/iptables.ipv4.nat
+.
+wq
+EOF
+
 sudo ed /etc/dnsmasq.conf << EOF
 $
 a
